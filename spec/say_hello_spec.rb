2704572ceadcb0b09b8f0eeed85a4772091e50ad
Ian  Jenkins <ianjerodjenkins@gmail.com>
2018-02-18 20:54:04 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -2,8 +2,6 @@ require_relative './spec_helper'
 
 describe "say_hello" do
 
-  def say_hello()
-
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
